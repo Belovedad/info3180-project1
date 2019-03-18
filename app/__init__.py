bcf353b7a21ad687f45b0dabf0c82b19e9349510
Daniel Dormer <daniel.dormer@mymona.uwi.edu>
2019-03-18 03:11:25 +0000

    final?

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ app.config.from_object(__name__)
 
 app.config['UPLOAD_FOLDER'] = "./app/static/uploads"
 app.config['SECRET_KEY'] = "random"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://pro1:pro1@localhost/pro1"
-#app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://zleemoztxzukqn:aa2a37bbc783f15ce145e52e75b6ca4ad371b6ff4011bc00ad6873939b708797@ec2-75-101-133-29.compute-1.amazonaws.com:5432/d9lrtd31pctolg'
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://pro1:pro1@localhost/pro1"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bhelaruohnruww:b156672b02fac7d95779fd2ce642174fce05c44779183f52379bf947a85af1a2@ec2-23-23-195-205.compute-1.amazonaws.com:5432/dapv42qhjrsc6i'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
